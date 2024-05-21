1341b6d84e2f395339c6edde4c7244c2e53caad8
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 17:27:18 -0300

    test: adding tests for load taskform and tasklist components

--- cypress/e2e/main_page_load.cy.ts
+++ cypress/e2e/main_page_load.cy.ts
@@ -8,4 +8,22 @@ describe("Main Page Load", () => {
     cy.get("main").should("exist");
     cy.get("footer").should("exist");
   });
+
+  it("should load TaskForm component", () => {
+    cy.get("#task-form").should("exist");
+    cy.get("h2").contains("O que você vai fazer?").should("exist");
+    cy.get("#task-form").within(() => {
+      cy.get("label").contains("Título").should("exist");
+    });
+    cy.get("input[name=title]").should("exist");
+    cy.get("label").contains("Dificuldade").should("exist");
+    cy.get("input[name=difficulty]").should("exist");
+    cy.get("input[type=submit]").contains("Criar tarefa").should("exist");
+  });
+
+  it("should load TaskList component", () => {
+    cy.get("#task-list").should("exist");
+    cy.get("h2").contains("Suas tarefas").should("exist");
+    cy.get("p").contains("Não há tarefas cadastradas!").should("exist");
+  });
 });
--- cypress/integration/main_page_load.feature
+++ cypress/integration/main_page_load.feature
@@ -5,3 +5,19 @@ Feature: Main Page Load
     Then I should see the header section
     And I should see the main section
     And I should see the footer section
+
+   Scenario: Load TaskForm component
+    Given I visit the main page
+    Then I should see the TaskForm component
+    And I should see an h2 with content "O que você vai fazer?"
+    And I should see a form with:
+      | label       | input        |
+      | title       | input[type=text][name=title] |
+      | difficulty  | input[type=text][name=difficulty] |
+      | submit      | input[type=submit][value=Criar tarefa] |
+
+   Scenario: Load TaskList component
+    Given I visit the main page
+    Then I should see the TaskList component
+    And I should see an h2 with content "Suas tarefas"
+    And I should see an p with content "Não há tarefas cadastradas!"
