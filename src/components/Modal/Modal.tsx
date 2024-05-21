d8c5024225032a01d01c9dbd2922b1bbd19ec803
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 15:39:18 -0300

    style: adding style to edit task modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,16 @@
 import styles from "./Modal.module.css";
 
-type Props = {};
+type Props = {
+  children: React.ReactNode;
+};
 
-function Modal({}: Props) {
+function Modal({ children }: Props) {
   return (
     <div id="modal">
-      <div></div>
-      <div>
+      <div className={styles.fade}></div>
+      <div className={styles.modal}>
         <h2>Texto Modal</h2>
+        {children}
       </div>
     </div>
   );
