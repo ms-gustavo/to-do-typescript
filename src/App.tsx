99ebd236b86b1f025c256b49b13f1e3b08ed758f
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 14:09:46 -0300

    creating header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>To Do React</>;
+  return (
+    <>
+      <Header />
+      <h1>Conteúdo...</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo</span> @ 2024
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
