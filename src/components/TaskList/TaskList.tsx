ece2e5f7eb73618a45500897ac748ba8bcd62c50
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 15:25:23 -0300

    style: styling task list

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -11,12 +11,12 @@ function TaskList({ taskList }: Props) {
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <div>
+          <div className={styles.task} key={task.id}>
+            <div className={styles.details}>
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
+            <div className={styles.actions}>
               <i className="bi bi-pencil"></i>
               <i className="bi bi-trash"></i>
             </div>
