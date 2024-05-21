29607c41b438349d5ad34a7e1964ba0c603523b2
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 14:56:21 -0300

    style: adding styles to task form

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-
+import styles from "./TaskForm.module.css";
 type Props = {
   btnText: string;
 };
 
 function TaskForm({ btnText }: Props) {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label htmlFor="title">Título:</label>
         <input type="text" name="title" placeholder="Título da tarefa" />
       </div>
-      <div>
+      <div className={styles.input_container}>
         <label htmlFor="difficulty">Dificuldade:</label>
         <input
           type="text"
