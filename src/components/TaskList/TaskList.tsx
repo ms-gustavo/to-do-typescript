89d322111abe1ac839b7bb1be185fcc84bfeb09c
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 15:20:21 -0300

    style: html for item list

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -12,11 +12,18 @@ function TaskList({ taskList }: Props) {
       {taskList.length > 0 ? (
         taskList.map((task) => (
           <div key={task.id}>
-            <p>{task.title}</p>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
           </div>
         ))
       ) : (
-        <p>Não há tarefas cadastradas</p>
+        <p>Não há tarefas cadastradas!</p>
       )}
     </>
   );
