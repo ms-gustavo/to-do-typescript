82a0395f7bdd50f038364e7a050e9a5499804ed6
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 14:35:48 -0300

    feat: creating form for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import styles from "./App.module.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import TaskForm from "./components/TaskForm/TaskForm";
+import TaskList from "./components/TaskList/TaskList";
 
 function App() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h1>Conteúdo...</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </>
---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function TaskList({}: Props) {
+  return (
+    <div>TaskList</div>
+  )
+}
+
+export default TaskList
