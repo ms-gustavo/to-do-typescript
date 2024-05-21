0f988f44994adc8aba8a778522069ea09ab987fa
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-21 15:28:08 -0300

    feat: deleting tasks

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -4,9 +4,10 @@ import { ITask } from "../../interfaces/Task";
 
 type Props = {
   taskList: ITask[];
+  handleDelete(id: number): void;
 };
 
-function TaskList({ taskList }: Props) {
+function TaskList({ taskList, handleDelete }: Props) {
   return (
     <>
       {taskList.length > 0 ? (
@@ -18,7 +19,12 @@ function TaskList({ taskList }: Props) {
             </div>
             <div className={styles.actions}>
               <i className="bi bi-pencil"></i>
-              <i className="bi bi-trash"></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
             </div>
           </div>
         ))
